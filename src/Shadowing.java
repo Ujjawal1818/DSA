6de23f5fed0141bd73e901b729cef8632e831454
Ujjawal1818 <anandricky38@gmail.com>
2026-08-24 23:21:53 +0530

    Code for Function 3.

---
+++ src/Shadowing.java
@@ -0,0 +1,13 @@
+public class Shadowing {
+             static int x=99;
+             public static void main(String[] args) {
+                 System.out.println(x);
+                 int x=50;
+                 System.out.println(x);
+                 fun();
+             }
+             static void fun() {
+                 System.out.println(x);
+             }
+}
+

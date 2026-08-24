1effd79b6e01e6f3bb15e99ba535a48704daa442
Ujjawal1818 <anandricky38@gmail.com>
2026-08-24 23:28:26 +0530

    Code for Function 4.

---
+++ src/varArgs.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class varArgs {
+  public static void main(String[] args) {
+      fun(2, 3, 4, 5, 6, 7, 8, 9);
+  }
+  static void fun(int ...v){
+      System.out.println(Arrays.toString(v));
+  }
+}

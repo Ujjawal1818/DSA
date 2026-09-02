e1827b92cb5680136a5a6bedcc6ae75e422b280f
Ujjawal1818 <anandricky38@gmail.com>
2026-09-02 19:59:04 +0530

    Basic squaring of a number.

---
+++ src/square.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class square {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sq;
+        int a =sc.nextInt();
+
+        sq=a*a;
+        System.out.println("The square of "+a+" is = "+sq);
+    }
+}

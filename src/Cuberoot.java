1597f76bffd01a3785f92d632a957b3800c1f955
Ujjawal1818 <anandricky38@gmail.com>
2026-09-03 10:02:59 +0530

    Basic cube of a number.

---
+++ src/Cuberoot.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Cuberoot {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cu;
+        int a =sc.nextInt();
+
+        cu=a*a*a;
+        System.out.println("The cube of "+a+" is = "+cu);
+    }
+
+}
